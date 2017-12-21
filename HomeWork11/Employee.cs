f479978d92414c8aa6626a594f0afab004b90ca9
Dias29 <33831288+Dias29@users.noreply.github.com>
2017-12-22 03:21:11 +0600

    first

---
+++ HomeWork11/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork11
+{
+    public class Employee
+    {
+        private string FName { get; set; }
+        private string LName { get; set; }
+        private int Salary { get; set; }
+        private DateTime Recruit { get; set; }
+        PostEnum _post;
+
+        private int Day { get; set; }
+        private int Month { get; set; }
+        private int Year { get; set; }
+
+        public Employee(string fname, string lname, PostEnum post, int salary , int year, int month, int day)
+        {
+            FName = fname;
+            LName = lname;
+            _post = post;
+            Salary = salary;
+            Recruit = new DateTime(year, month, day);
+        }
+
+        
+    }
+}
---
+++ HomeWork11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employee = new Employee("", "",PostEnum.Boss ,0, 0000, 00, 00);
+        }
+    }
+}
